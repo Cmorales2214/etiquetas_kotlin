3b1938826743bd5d39f680641e6d33c29255f9fd
bjimenez40793 <80145595+bjimenez40793@users.noreply.github.com>
2021-07-26 23:23:01 -0600

    Stings1

--- Proyecto/app/src/main/java/com/sc703/proyecto/MainActivity.kt
+++ Proyecto/app/src/main/java/com/sc703/proyecto/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.sc703.proyecto
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val  Codigo_Solicitud = 1234
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        SolicitarPermisos()
+    }
+
+    private fun SolicitarPermisos(){
+        val Telefono = ActivityCompat.checkSelfPermission(this@MainActivity,
+            android.Manifest.permission.CALL_PHONE)
+        val Internet = ActivityCompat.checkSelfPermission(this@MainActivity,
+            android.Manifest.permission.INTERNET)
+
+        if (Telefono != PackageManager.PERMISSION_GRANTED ||
+            Internet != PackageManager.PERMISSION_GRANTED){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(arrayOf(android.Manifest.permission.CALL_PHONE,
+                    android.Manifest.permission.INTERNET),Codigo_Solicitud)
+            }
+        }
     }
 }
