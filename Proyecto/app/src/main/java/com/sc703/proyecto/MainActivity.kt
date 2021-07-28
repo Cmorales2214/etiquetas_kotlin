f81d6288361d64f0eda0fe218cdf77b47330da0a
Ahernandez2904 <hernandezlara180@gmail.com>
2021-07-28 00:02:20 -0600

    Se arregló que la aplicación no iniciaba
    late init no funcionó y se le quitaron los errores al poner las dependencias de otro proyecto

--- Proyecto/app/src/main/java/com/sc703/proyecto/MainActivity.kt
+++ Proyecto/app/src/main/java/com/sc703/proyecto/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         SolicitarPermisos()
-
+        btn_Registrar = findViewById(R.id.btn_Registrar)
         //Se le da la funcionalidad del boton de navegar a Registrar usuario
         btn_Registrar.setOnClickListener {
             val intent: Intent = Intent( this, RegistrarUsuario::class.java)
