c74e0c3f2b17fb587beb2c8c6bb3435a4ca4fb61
CharlesRz663 <43615588+Cmorales2214@users.noreply.github.com>
2021-08-03 06:16:33 -0600

    Fragments salir y contactos
    Se agrega el fragment salir y el fragments contactos

--- etiquetas_kotlin/app/src/main/java/com/sc703/etiquetas_kotlin/MainActivity.kt
+++ etiquetas_kotlin/app/src/main/java/com/sc703/etiquetas_kotlin/MainActivity.kt
@@ -1,21 +1,45 @@
 package com.sc703.etiquetas_kotlin
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
 
 
     private val  Codigo_Solicitud = 1234
+    // Intanciamos el autententicador
+    private lateinit var auth: FirebaseAuth
 
 override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    auth = FirebaseAuth.getInstance()
+
 }
+    //Mantenemos la sesion iniciada
+    override fun onStart() {
+        super.onStart()
+        val User = auth.currentUser
+        Toast.makeText(applicationContext, "Inicio de sesion correcto", Toast.LENGTH_SHORT).show()
+        ActualizarInterfaz(User)
+    }
+
+    //Metodo para cambiar de intent (formulario)
+    private fun ActualizarInterfaz(user: FirebaseUser?) {
+        if (user != null) {
+            val intent = Intent(this, ActivityPrincipal::class.java)
+            startActivity(intent)
+        }
+    }
+
 
     private fun SolicitarPermisos(){
         val Telefono = ActivityCompat.checkSelfPermission(this@MainActivity,
