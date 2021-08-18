ed60efebdbc1ec09157ded87ed279576475922a1
CharlesRz663 <43615588+Cmorales2214@users.noreply.github.com>
2021-08-18 14:57:25 -0600

    Banner
    Se agrega banner en la session de recuperar contraseña

--- etiquetas_kotlin/app/src/main/java/com/sc703/etiquetas_kotlin/RecuperarPassActivity.kt
+++ etiquetas_kotlin/app/src/main/java/com/sc703/etiquetas_kotlin/RecuperarPassActivity.kt
@@ -8,6 +8,9 @@ import android.view.View
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.google.firebase.auth.FirebaseAuth
 
 class RecuperarPassActivity : AppCompatActivity() {
@@ -17,6 +20,9 @@ class RecuperarPassActivity : AppCompatActivity() {
 
     private lateinit var progressBar : ProgressBar
 
+    //Variable para anuncio
+    private lateinit var av_Banner: AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recuperar_pass)
@@ -24,6 +30,15 @@ class RecuperarPassActivity : AppCompatActivity() {
         txtEmail = findViewById(R.id.txtEmail)
         auth = FirebaseAuth.getInstance()
         progressBar = findViewById(R.id.progressBar3)
+
+
+        //Inicializar anuncios
+        MobileAds.initialize(this){}
+        //Solicitar bloque de anuncio
+        val solicitud_Anuncio = AdRequest.Builder().build()
+        av_Banner = findViewById(R.id.av_banner)
+        //Pasamos el bloque de anuncio a Adview
+        av_Banner.loadAd(solicitud_Anuncio)
     }
 
     fun enviar(view: View){
