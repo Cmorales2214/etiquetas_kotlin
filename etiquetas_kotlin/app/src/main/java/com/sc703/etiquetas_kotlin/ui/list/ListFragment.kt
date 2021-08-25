e2c35ba4df10ca55f8ddbb7657658e45b87dfff7
Ahernandez2904 <hernandezlara180@gmail.com>
2021-08-25 11:20:01 -0600

    delete sqllite for recycler view

--- etiquetas_kotlin/app/src/main/java/com/sc703/etiquetas_kotlin/ui/list/ListFragment.kt
+++ etiquetas_kotlin/app/src/main/java/com/sc703/etiquetas_kotlin/ui/list/ListFragment.kt
@@ -1,7 +1,5 @@
 package com.sc703.etiquetas_kotlin.ui.list
 
-import android.app.AlertDialog
-import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.util.Base64
 import androidx.fragment.app.Fragment
@@ -13,8 +11,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.FirebaseFirestore
 import com.sc703.etiquetas_kotlin.R
-import com.sc703.etiquetas_kotlin.ui.etiqueta.EtiquetaFragment
-import com.sc703.etiquetas_kotlin.ui.sqlite.ModeloSQL
 import java.security.MessageDigest
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
@@ -22,87 +18,44 @@ import javax.crypto.spec.SecretKeySpec
 class ListFragment : Fragment() {
 
     private val llave_Encriptacion = "12354-*-/werfmksdfSDF23$%^&*=="
-
     private lateinit var recyclerView: RecyclerView
-
     private lateinit var adaptador: Adaptador
-    private lateinit var modeloSQL: ModeloSQL
-    private lateinit var bd_SQLite: SQLiteDatabase
-    //private lateinit var dialogo: AlertDialog
-    //private lateinit var bulder: AlertDialog.Builder
-
     private val lista = ArrayList<Etiquetas>()
-
-    var id: String? = null
-    var cierre: String? = null
-    var color: String? = null
-    var descripcion: String? = null
-    var emision: String? = null
-    var linea: String? = null
-    var nombre: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_list, container, false)
-        modeloSQL = ModeloSQL(context, "etiquetas", null, 1)
         recyclerView = root.findViewById(R.id.RVrecyclerview)
         recyclerView.layoutManager = LinearLayoutManager(context)
-        adaptador = Adaptador(lista)
+        adaptador = Adaptador(lista, context)
         recyclerView.adapter = adaptador
 
-        CargaDatos2()
+        CargaDatos()
         return root
     }
 
-    private fun CargaDatos2(){
+    private fun CargaDatos(){
         val db: FirebaseFirestore = FirebaseFirestore.getInstance()
 
         db.collection("etiquetas").get().addOnSuccessListener { documento ->
             for(document in documento) {
                 val e = Etiquetas()
-                e.id = "${document.id}"
-                e.cierre = "${document.data.get("cierre")}"
-                e.color = "${document.data.get("color")}"
-                e.descripcion = "${document.data.get("descripcion")}"
-                e.emision = "${document.data.get("emision")}"
-                e.linea = "${document.data.get("linea")}"
-                e.nombre = "${document.data.get("nombre")}"
+                e.id = Desencriptar("${document.id}", llave_Encriptacion)
+                e.cierre = Desencriptar("${document.data.get("cierre")}", llave_Encriptacion)
+                e.color = Desencriptar("${document.data.get("color")}", llave_Encriptacion)
+                e.descripcion = Desencriptar("${document.data.get("descripcion")}", llave_Encriptacion)
+                e.emision = Desencriptar("${document.data.get("emision")}", llave_Encriptacion)
+                e.linea = Desencriptar("${document.data.get("linea")}", llave_Encriptacion)
+                e.nombre = Desencriptar("${document.data.get("nombre")}", llave_Encriptacion)
                 lista.add(e)
                 adaptador.notifyDataSetChanged()
-                //Toast.makeText(context, "${document.id}", Toast.LENGTH_SHORT).show()
             }
         }
         .addOnFailureListener { exception ->
             Toast.makeText(context, R.string.tag_fail, Toast.LENGTH_SHORT).show()
         }
-
-            /*if(documento.exists()){
-                    val color:String? = Desencriptar(documento.getString("color").toString(), llave_Encriptacion)
-                    val operador:String? = Desencriptar(documento.getString("nombre").toString(), llave_Encriptacion)
-                    val lineaproducc:String? = Desencriptar(documento.getString("linea").toString(), llave_Encriptacion)
-                    val descripcion:String? = Desencriptar(documento.getString("descripcion").toString(), llave_Encriptacion)
-                    val emission:String? = Desencriptar(documento.getString("emision").toString(), llave_Encriptacion)
-                    val dateclose:String? = Desencriptar(documento.getString("cierre").toString(), llave_Encriptacion)
-                    var i = 0
-                    spn_TAGcolor.setSelection(0)
-                    selected_color = resources.getStringArray(R.array.tag_colors)[0].toString()
-                    resources.getStringArray(R.array.tag_colors).forEach {
-                        if (resources.getStringArray(R.array.tag_colors)[i].toString() == color.toString()) {
-                            spn_TAGcolor.setSelection(i)
-                            selected_color = resources.getStringArray(R.array.tag_colors)[i].toString()
-                        }
-                        i++
-                    }
-                    edt_TAGoperator.setText(operador)
-                    edt_TAGline.setText(lineaproducc)
-                    edt_TAGdescription.setText(descripcion)
-                    edt_TAGemission.setText(emission)
-                    edt_TAGclose.setText(dateclose)
-                }else{
-                    Toast.makeText(context, R.string.tag_not_found, Toast.LENGTH_SHORT).show()
-                }*/
     }
 
     private fun Desencriptar(txt_Desencriptar: String, Clave: String): String {
@@ -119,36 +72,5 @@ class ListFragment : Fragment() {
         var clave: ByteArray? = Llave.toByteArray(charset("UTF-8"))
         clave = SHA_256.digest(clave)
         return SecretKeySpec(clave, "AES")
-    }
-
-    private fun CargaDatos() {
-        bd_SQLite = modeloSQL.readableDatabase
-        val fila = bd_SQLite.rawQuery("SELECT * FROM etiquetas", null)
-        for (i in 0 until fila.count) {
-            val e = Etiquetas()
-
-            if (fila.moveToPosition(i)) {
-                id = fila.getString(0).toString()
-                cierre = fila.getString(1).toString()
-                color = fila.getString(2).toString()
-                descripcion = fila.getString(3).toString()
-                emision = fila.getString(4).toString()
-                linea = fila.getString(5).toString()
-                nombre = fila.getString(6).toString()
-
-                e.id = id
-                e.cierre = cierre
-                e.color = color
-                e.descripcion = descripcion
-                e.emision = emision
-                e.linea = linea
-                e.nombre = nombre
-                lista.add(e)
-                adaptador.notifyDataSetChanged()
-            } else {
-                Toast.makeText(context, R.string.tag_fail, Toast.LENGTH_SHORT).show()
-            }
-        }
-        bd_SQLite.close()
     }
 }
